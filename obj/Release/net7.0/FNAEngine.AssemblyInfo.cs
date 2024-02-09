c16d864c0a3d1a845a88a0a531fa1e3e18ce7e70
StepingForward <stepan.aleksanian@gmail.com>
2024-02-09 18:47:33 +0200

    Removed some stuff :p

--- obj/Release/net7.0/FNAEngine.AssemblyInfo.cs
+++ obj/Release/net7.0/FNAEngine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FNAEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b12502005864578077e64e81a73caf9c809d7c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FNAEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FNAEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
